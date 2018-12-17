7fe4e2fb148262dc39825edbe169aed0fd5c2adf
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-12-17 17:08:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,14 +3,14 @@
 
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
-    create_table :haunted_houses do |h|
-      c.string :name
-      c.string :location
-      c.integer :costume_invetory
-      c.integer :employee_number
-      c.boolean :business?
-      c.datetime :opening
-      c.datetime :closing
+    create_table :haunted_houses do |hh|
+      hh.string :name
+      hh.string :location
+      hh.integer :costume_invetory
+      hh.integer :employee_number
+      hh.boolean :business?
+      hh.datetime :opening
+      hh.datetime :closing
     end
   end
 end
